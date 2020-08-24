2fc375eefe4892703a58375e179bb7bbd6b14698
Vinayak Nayak <67859336+Vinayak-Nayak@users.noreply.github.com>
2020-08-24 16:36:50 +0530

    Add files via upload

---
+++ signup-login form/form.js
@@ -0,0 +1,8 @@
+function hidelog(){
+    document.getElementById('log').style.display= "none";
+    document.getElementById('sp').style.display="block";
+}
+function hidesp(){
+    document.getElementById('sp').style.display= "none";
+    document.getElementById('log').style.display="block";
+}
